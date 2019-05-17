bc64a8ca1764cacaf92a6e97232681c99443761b
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-17 16:56:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,7 @@ def countdown(num)
     while num < 10 
       puts "#{num} SECOND(S)!"
       num -= 10
+      return "HAPPY NEW YEAR!"
     end
-    return "HAPPY NEW YEAR!"
+    # return "HAPPY NEW YEAR!"
 end
