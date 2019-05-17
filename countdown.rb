b4b3fe4117c8d422ca806305c32de12dc68d05cc
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-17 17:01:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
-def countdown(number)
-    while num < number 
+def countdown(num)
+    while num < 10 
       puts "#{num} SECOND(S)!"
       num -= 1
       return "HAPPY NEW YEAR!"
