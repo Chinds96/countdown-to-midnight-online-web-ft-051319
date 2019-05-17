6ba511014900c7775f432e39d9fc66a85ba99d42
Christopher Hinds <christopherhinds96@gmail.com>
2019-05-17 17:14:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,11 @@ def countdown(num)
     while num > 0 
       puts "#{num} SECOND(S)!"
       num -= 1
-      countdown_with_sleep()
+      countdown_with_sleep(1)
     end
     return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep()
-  sleep(1)
+  sleep()
 end
